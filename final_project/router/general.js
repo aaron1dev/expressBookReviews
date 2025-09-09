9b580057a1adb32f9d2432fdd0545f108e6dfc10
Your Name <you@example.com>
2025-09-08 23:42:24 -0400

    search by isbn

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -28,13 +28,25 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+      // Send JSON response with formatted friends data
+      return res.send(JSON.stringify(books,null,4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    let isbn = req.params.isbn;
+    
+    if(isbn)
+    {
+        let book = books[ parseInt(isbn) ];
+        if(book)
+        {
+            return res.send(JSON.stringify(book,null,4));
+        }
+    }
+
+    return res.status(404).json({message: "Book not found "});
  });
   
 // Get book details based on author
